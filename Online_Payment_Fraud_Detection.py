fe4a524bcf3f07bbfc4d2c118a85d46d11faa0b2
Sammybams <samuelbamgbola@gmail.com>
2023-07-08 18:07:52 +0100

    Added input widgets for step.

--- Online_Payment_Fraud_Detection.py
+++ Online_Payment_Fraud_Detection.py
@@ -5,14 +5,16 @@ st.title('Online Payment Fraud Detection')
 
 fraud_case = {0:" No Fraud Detected",
               1:"Fraud Detected"}
+
 type_map = {"PAYMENT":"PAYMENT",
             "TRANSFER":"TRANSFER",
             "CASH OUT":"CASH_OUT",
             "DEBIT":"DEBIT",
-            "CASH IN":"CASH OUT",
+            "CASH IN":"CASH OUT"}
 
-}
+step = st.number_input('Enter window of transaction (in hours)', min_value=1, max_value=10)
 
 type = st.selectbox(
-    'Select Disorder Type',
-    (sorted(crime.DISORDER_TYPE.unique())))
+    'Select Transaction Type',
+    (type_map.keys()))
+
