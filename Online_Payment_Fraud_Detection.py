e26e07d336fe246d4dbbbff15cd0b9533a7b4d8b
Sammybams <samuelbamgbola@gmail.com>
2023-07-08 17:33:28 +0100

    Added dictionary to map output.

--- Online_Payment_Fraud_Detection.py
+++ Online_Payment_Fraud_Detection.py
@@ -2,3 +2,7 @@ import streamlit as st
 st.set_page_config(page_title="Online Payment Fraud Detection App", page_icon="💳")
 
 st.title('Online Payment Fraud Detection')
+
+fraud_case = {0:" No Fraud Detected",
+              1:"Fraud Detected"}
+
