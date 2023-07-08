095225ed10be2cbacb69a69f27ce3970226dd33d
Sammybams <samuelbamgbola@gmail.com>
2023-07-08 17:56:01 +0100

    Made changes to page script.

--- Online_Payment_Fraud_Detection.py
+++ Online_Payment_Fraud_Detection.py
@@ -5,4 +5,14 @@ st.title('Online Payment Fraud Detection')
 
 fraud_case = {0:" No Fraud Detected",
               1:"Fraud Detected"}
+type_map = {"PAYMENT":"PAYMENT",
+            "TRANSFER":"TRANSFER",
+            "CASH OUT":"CASH_OUT",
+            "DEBIT":"DEBIT",
+            "CASH IN":"CASH OUT",
 
+}
+
+type = st.selectbox(
+    'Select Disorder Type',
+    (sorted(crime.DISORDER_TYPE.unique())))
