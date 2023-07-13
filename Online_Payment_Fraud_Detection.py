dfd7f5f5bb474435c49dce94b1fccf03d3d7e8ed
Sammybams <samuelbamgbola@gmail.com>
2023-07-13 10:08:40 +0100

    Added some white spaces.

--- Online_Payment_Fraud_Detection.py
+++ Online_Payment_Fraud_Detection.py
@@ -82,38 +82,38 @@ body = str.encode(json.dumps(data))
 if st.button("Run"):
     st.header("Prediction")
 
-    # body = str.encode(json.dumps(data))
+    body = str.encode(json.dumps(data))
 
-    # url = os.environ.get('ENDPOINT')
-    # # Replace this with the primary/secondary key or AMLToken for the endpoint
-    # api_key = os.environ.get('KEY')
-    # if not api_key:
-    #     raise Exception("A key should be provided to invoke the endpoint")
+    url = os.environ.get('ENDPOINT')
+    # Replace this with the primary/secondary key or AMLToken for the endpoint
+    api_key = os.environ.get('KEY')
+    if not api_key:
+        raise Exception("A key should be provided to invoke the endpoint")
 
 
-    # headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
-    # req = urllib.request.Request(url, body, headers)
+    req = urllib.request.Request(url, body, headers)
 
-    # try:
-    #     response = urllib.request.urlopen(req)
-
-    #     result = response.read()
-    #     #print(result)
-
-    #     #print(int("".join(result.decode().split()[1])[1]))
-    #     prediction = int("".join(result.decode().split()[1])[1])
-
-    # except urllib.error.HTTPError as error:
-    #     print("The request failed with status code: " + str(error.code))
-
-    #     # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-    #     #print(error.info())
-    #     #print(error.read().decode("utf8", 'ignore'))
-    #     prediction = error.read().decode("utf8", 'ignore')
-
-    prediction = "123"
     try:
+        response = urllib.request.urlopen(req)
+
+        result = response.read()
+        #print(result)
+
+        #print(int("".join(result.decode().split()[1])[1]))
+        prediction = int("".join(result.decode().split()[1])[1])
+
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        #print(error.info())
+        #print(error.read().decode("utf8", 'ignore'))
+        prediction = error.read().decode("utf8", 'ignore')
+    
+    try:
+
       output = fraud_case[prediction]
       if prediction:
           st.warning(f'{output}', icon="⚠️")
