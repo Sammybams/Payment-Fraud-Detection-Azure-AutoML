988a468a6f901b2eb63078786b5cb71c27e9a72c
Sammybams <samuelbamgbola@gmail.com>
2023-07-13 11:13:25 +0100

    Corrected condition to check if CSV file.

--- Online_Payment_Fraud_Detection.py
+++ Online_Payment_Fraud_Detection.py
@@ -175,7 +175,7 @@ st.divider()
 st.subheader("Batch Prediction")
 uploaded_file = st.file_uploader("Choose a CSV file")
 if uploaded_file is not None:
-    if uploaded_file.name[-4:]!=',csv':
+    if uploaded_file.name[-4:]!='.csv':
         st.warning(f'Not a CSV file', icon="⚠️")
         
     else:
